7977c425d87424d434cb97ea81814bf3d02de177
rxnzu <markjuliusgalvez@gmail.com>
2026-09-23 07:47:20 +0800

    Update game source code and add Electron builder config

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 720,
+    resizable: true,
+    autoHideMenuBar: true,
+    title: "Blockdrop",
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
